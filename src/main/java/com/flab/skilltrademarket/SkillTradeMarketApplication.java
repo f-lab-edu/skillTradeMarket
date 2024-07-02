8cd629e5ae3a3beadf04aeda16a0027edd48e28f
김병조 <belongs2jo@gmail.com>
2024-07-02 14:37:32 +0900

    create project

---
+++ src/main/java/com/flab/skilltrademarket/SkillTradeMarketApplication.java
@@ -0,0 +1,13 @@
+package com.flab.skilltrademarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTradeMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillTradeMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/skilltrademarket/SkillTradeMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.skilltrademarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTradeMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
