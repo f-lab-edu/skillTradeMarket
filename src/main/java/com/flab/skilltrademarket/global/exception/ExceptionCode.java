d9ad37660888180925d900549da8985addf174da
김병조 <belongs2jo@gmail.com>
2024-07-23 16:11:15 +0900

    feat : ExceptionCode 추가

--- src/main/java/com/flab/skilltrademarket/global/exception/ExceptionCode.java
+++ src/main/java/com/flab/skilltrademarket/global/exception/ExceptionCode.java
@@ -9,10 +9,12 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionCode {
     // User
     NOT_FOUND_EMAIL("U001","이메일은 필수 입력 정보입니다.","이메일은 필수 입력 정보입니다.",HttpStatus.BAD_REQUEST),
-    NOT_FOUND_NICKNAME("U001","닉네임은 필수 입력 정보입니다.","닉네임은 필수 입력 정보입니다.",HttpStatus.BAD_REQUEST),
-    VALIDATE_EMAIL("U001","이메일 형식이 맞지 않습니다.","잘못된 형식입니다.",HttpStatus.BAD_REQUEST),
-    VALIDATE_NICKNAME("U002","닉네임 형식이 맞지 않습니다.","잘못된 형식입니다.",HttpStatus.BAD_REQUEST),
-    VALIDATE_PHONE_NUM("U003","전화번호 형식이 맞지 않습니다.","잘못된 형식입니다.",HttpStatus.BAD_REQUEST),
+    NOT_FOUND_NICKNAME("U002","닉네임은 필수 입력 정보입니다.","닉네임은 필수 입력 정보입니다.",HttpStatus.BAD_REQUEST),
+    VALIDATE_EMAIL("U003","이메일 형식이 맞지 않습니다.","잘못된 형식입니다.",HttpStatus.BAD_REQUEST),
+    VALIDATE_NICKNAME("U004","닉네임 형식이 맞지 않습니다.","잘못된 형식입니다.",HttpStatus.BAD_REQUEST),
+    VALIDATE_PHONE_NUM("U005","전화번호 형식이 맞지 않습니다.","잘못된 형식입니다.",HttpStatus.BAD_REQUEST),
+    LOGIN_FAIL("U006","아이디,비밀번호를 확인해 주세요.","아이디,비밀번호를 확인해 주세요.",HttpStatus.BAD_REQUEST),
+    NOT_FOUND("U007","존재하지 않는 사용자입니다.","존재하지 않는 사용자입니다.", HttpStatus.NOT_FOUND),
     DUPLICATE_EMAIL("U011","중복된 이메일[%s]입니다.","사용할 수 없는 이메일입니다.", HttpStatus.BAD_REQUEST),
     DUPLICATE_NICKNAME("U012","중복된 닉네임[%s]입니다.","사용할 수 없는 닉네임입니다.", HttpStatus.BAD_REQUEST),
     INVALID_PW_CONFIRM("U013", "비밀번호가 일치하지 않습니다.", "비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
@@ -21,8 +23,9 @@ public enum ExceptionCode {
     USER_AUTH_PHONE_CODE_DIFF("U022","인증번호가 일치하지 않습니다.","인증번호가 일치하지 않습니다.",HttpStatus.BAD_REQUEST),
 
     //
-    COMMON_SYSTEM_ERROR("C001","시스템 에러입니다.","시스템 에러입니다.",HttpStatus.BAD_REQUEST);
-
+    COMMON_SYSTEM_ERROR("C001","시스템 에러입니다.","시스템 에러입니다.",HttpStatus.BAD_REQUEST),
+    ACCESS_DENIED("C002","인증이 필요합니다.","인증이 필요합니다.", HttpStatus.UNAUTHORIZED),
+    EXPIRED_TOKEN("C003","만료된 토큰 입니다.","만료된 토큰 입니다.", HttpStatus.UNAUTHORIZED);
     private final String code;
     private final String internalMessage;
     private final String externalMessage;
