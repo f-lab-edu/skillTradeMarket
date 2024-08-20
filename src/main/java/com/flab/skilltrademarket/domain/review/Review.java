f40018c4d89b32e6d95f55acd2410aa59296bb81
김병조 <belongs2jo@gmail.com>
2024-08-20 13:32:12 +0900

    feat : 리뷰, 답장 엔티티 추가

--- src/main/java/com/flab/skilltrademarket/domain/review/Review.java
+++ src/main/java/com/flab/skilltrademarket/domain/review/Review.java
@@ -3,8 +3,10 @@ package com.flab.skilltrademarket.domain.review;
 import com.flab.skilltrademarket.domain.category.SubCategory;
 import com.flab.skilltrademarket.domain.common.BaseTimeEntity;
 import com.flab.skilltrademarket.domain.expert.Expert;
+import com.flab.skilltrademarket.domain.reply.Reply;
 import com.flab.skilltrademarket.domain.user.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -31,10 +33,27 @@ public class Review extends BaseTimeEntity {
     @JoinColumn(name = "sub_category_id")
     private SubCategory subCategory;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reply_id")
+    private Reply reply;
+
     private String content;
 
     private double rating;
 
 
+    @Builder
+    public Review(User writer, Expert expert, SubCategory subCategory, String content, int rating) {
+        this.writer = writer;
+        this.expert = expert;
+        this.subCategory = subCategory;
+        this.content = content;
+        this.rating = rating;
+
+    }
+
+    public void addReply(Reply reply) {
+        this.reply = reply;
+    }
 
 }
